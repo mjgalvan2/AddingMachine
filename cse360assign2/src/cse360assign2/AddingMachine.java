c1031f4a1d20726cb5da295d5a3c453668a507fd
mjgalvan2 <mjgalvan@asu.edu>
2019-10-09 11:39:44 -0700

    Initial version AddingMachine

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,45 @@
+/* Matthew Galvan
+ * 1206578967
+ * Homework 2
+ * CSE 360: W 9:40am-10:30am
+ * AddingMachine: This project is design to implement
+ * a basic calculator that can do basic addition and
+ * subtraction. This project is also supposed to 
+ * remember operations and also clear it. */
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	//constructor
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//getter method. returns the total
+	public int getTotal () {
+		return 0;
+	}
+	
+	//mutator method. adds to the total
+	public void add (int value) {
+		
+	}
+	
+	//mutator method. subtracts from the total
+	public void subtract (int value) {
+		
+	}
+	
+	//returns the operation history.
+	public String toString () {
+		return "";
+	}
+	
+	//mutator method. clears the operation history
+	public void clear() {
+	
+	}
+}
